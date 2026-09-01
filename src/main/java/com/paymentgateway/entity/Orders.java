d53c4452b71f30bbf8b2af6d8ac7ba7491adf035
prem-kumar3405 <pream342005@gmail.com>
2026-09-01 21:41:46 +0530

    created Order entityclass

---
+++ src/main/java/com/paymentgateway/entity/Orders.java
@@ -0,0 +1,45 @@
+package com.paymentgateway.entity;
+
+import com.paymentgateway.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Orders {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+    @JoinColumn(name = "merchant_id",nullable = false)
+    private Merchant merchant;
+
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+    @JoinColumn(name = "customer_id",nullable = false)
+    private Customer customer;
+
+    @Column(nullable = false,precision = 19,scale = 2)
+    private BigDecimal amount;
+
+    @Column(nullable = false,length = 3)
+    private String currency;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+}
