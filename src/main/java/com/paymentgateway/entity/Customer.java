ba1c5fc455a74c7117465f25af37251a6af1251c
prem-kumar3405 <pream342005@gmail.com>
2026-09-01 20:33:53 +0530

    created customer entity

---
+++ src/main/java/com/paymentgateway/entity/Customer.java
@@ -0,0 +1,29 @@
+package com.paymentgateway.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+}
