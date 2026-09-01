0db48752af87409ddb1bd5e2c89f40ad57fe0566
prem-kumar3405 <pream342005@gmail.com>
2026-09-01 22:59:58 +0530

    feat: add merchant not found exception

---
+++ src/main/java/com/paymentgateway/exception/MerchantNotFoundException.java
@@ -0,0 +1,8 @@
+package com.paymentgateway.exception;
+
+public class MerchantNotFoundException extends RuntimeException{
+
+    public MerchantNotFoundException(String message) {
+        super(message);
+    }
+}
