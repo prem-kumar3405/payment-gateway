400390d73f25e842cd18ffd90f815b6cbc963962
prem-kumar3405 <pream342005@gmail.com>
2026-09-01 23:32:56 +0530

    feat: add payment transaction lookup by payment

--- src/main/java/com/paymentgateway/repository/PaymentTransactionRepository.java
+++ src/main/java/com/paymentgateway/repository/PaymentTransactionRepository.java
@@ -3,6 +3,10 @@ package com.paymentgateway.repository;
 import com.paymentgateway.entity.PaymentTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PaymentTransactionRepository
         extends JpaRepository<PaymentTransaction, Long> {
+
+    List<PaymentTransaction> findByPaymentId(Long paymentId);
 }
