417300a256da3bf4f9c8f4b30f5e1961e3674f0c
prem-kumar3405 <pream342005@gmail.com>
2026-09-01 23:54:51 +0530

    feat: add latest transaction lookup by payment

--- src/main/java/com/paymentgateway/repository/PaymentTransactionRepository.java
+++ src/main/java/com/paymentgateway/repository/PaymentTransactionRepository.java
@@ -4,9 +4,11 @@ import com.paymentgateway.entity.PaymentTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PaymentTransactionRepository
         extends JpaRepository<PaymentTransaction, Long> {
 
     List<PaymentTransaction> findByPaymentId(Long paymentId);
+    Optional<PaymentTransaction> findFirstByPaymentIdOrderByCreatedAtDesc(Long paymentId);
 }
