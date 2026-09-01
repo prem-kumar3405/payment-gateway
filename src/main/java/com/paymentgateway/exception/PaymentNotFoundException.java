985b3170eae4b3e564da346d685e5844c4b24c10
prem-kumar3405 <pream342005@gmail.com>
2026-09-01 23:39:47 +0530

    feat: add payment not found exception

---
+++ src/main/java/com/paymentgateway/exception/PaymentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.paymentgateway.exception;
+
+public class PaymentNotFoundException extends RuntimeException {
+    public PaymentNotFoundException(String message) {
+        super(message);
+    }
+}
