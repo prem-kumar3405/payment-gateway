7c15da6ff40c2af16e9844cb7720c326e7a46f9f
prem-kumar3405 <pream342005@gmail.com>
2026-09-01 22:26:16 +0530

    feat: implement merchant creation service

---
+++ src/main/java/com/paymentgateway/service/MerchantService.java
@@ -0,0 +1,46 @@
+package com.paymentgateway.service;
+
+import com.paymentgateway.dto.CreateMerchantRequest;
+import com.paymentgateway.dto.MerchantResponse;
+import com.paymentgateway.entity.Merchant;
+import com.paymentgateway.repository.MerchantRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class MerchantService {
+
+    private final MerchantRepository merchantRepository;
+
+
+    public MerchantResponse createMerchant(CreateMerchantRequest request) {
+
+        if(merchantRepository.findByEmail(request.email()).isPresent()){
+            throw  new IllegalArgumentException("Merchant email already exists");
+        }
+
+        Merchant merchant = Merchant.builder()
+                .name(request.name())
+                .email(request.email())
+                .apiKey(generateApiKey())
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        Merchant savedMerchant = merchantRepository.save(merchant);
+        return new MerchantResponse(
+                savedMerchant.getId(),
+                savedMerchant.getName(),
+                savedMerchant.getEmail(),
+                savedMerchant.getApiKey(),
+                savedMerchant.getCreatedAt()
+        );
+    }
+    private String generateApiKey() {
+        return "pk_" + UUID.randomUUID();
+    }
+
+}
